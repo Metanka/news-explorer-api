b4332528541b1cd8a8430a87dcfc96ab80ab0b5d
Metanka <themetanka@gmail.com>
2020-11-11 21:11:09 +0300

    add validation2

--- app.js
+++ app.js
@@ -39,14 +39,14 @@ app.get('/crash-test', () => {
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    email: Joi.string().required().email({ minDomainSegments: 2, tlds: { allow: false } }),
+    email: Joi.string().required().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'ru'] } }),
     password: Joi.string().required().min(8),
   }),
 }), registerUser);
 // логин, получение токена
 app.post('/signin', celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required().email({ minDomainSegments: 2, tlds: { allow: false } } }),
+    email: Joi.string().required().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'ru'] } }),
     password: Joi.string().required().min(8),
   }),
 }), login);
