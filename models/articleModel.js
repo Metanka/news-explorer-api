8830903597fcc146f0a9bcac93c7b71cb71a2692
Metanka <themetanka@gmail.com>
2020-11-13 19:50:51 +0300

    change validation

--- models/articleModel.js
+++ models/articleModel.js
@@ -28,7 +28,7 @@ const articleSchema = new mongoose.Schema({
     validate: {
       validator(v) {
         // eslint-disable-next-line no-useless-escape
-        return /^((http|https)\:\/\/)(www\.)?([a-zA-Z0-9\-]{0,50}\.?){1,5}\/?([a-zA-Z0-9\=\?\.\-\#\/]{0,50}){0,50}?\.?$/.test(v);
+        return /^((http|https):\/\/)(www\.)?([A-Za-z0-9.-]{1,256})\.[A-Za-z]{2,10}([-a-zA-Z0-9@:%_+.~#?&/=]{1,256})?$/.test(v);
       },
     },
   },
@@ -38,7 +38,7 @@ const articleSchema = new mongoose.Schema({
     validate: {
       validator(v) {
         // eslint-disable-next-line no-useless-escape
-        return /^((http|https)\:\/\/)(www\.)?([a-zA-Z0-9\-]{0,50}\.?){1,5}\/?([a-zA-Z0-9\=\?\.\-\#\/]{0,50}){0,50}?\.?$/.test(v);
+        return /^((http|https):\/\/)(www\.)?([A-Za-z0-9.-]{1,256})\.[A-Za-z]{2,10}([-a-zA-Z0-9@:%_+.~#?&/=]{1,256})?$/.test(v);
       },
     },
   },
